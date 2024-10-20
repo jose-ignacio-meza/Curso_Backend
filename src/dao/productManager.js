f7d8fc08c780b521878c64d69864b6bbf870449c
josemeza <nachomezalk24@gmail.com>
2024-10-20 01:00:09 -0300

    funciona get, getby, post y put

--- src/dao/productManager.js
+++ src/dao/productManager.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 
 export class ProductManager{
+
     static #products= []
     static #path='./src/data/products.json'
 
@@ -14,22 +15,14 @@ export class ProductManager{
 
     static async getProducts(){
         let products = await fs.promises.readFile(this.#path, {encoding:'utf-8'})
-        console.log(products)
         return JSON.parse(products)
     }
 
-    static async setProducts(product={}){
-        //validation
-        console.log(product)
-        let products = await fs.promises.readFile(this.#path, {encoding:'utf-8'})
-        
-        return products
-    }
-
+    
     // Cargar productos desde el archivo JSON
-    async loadProducts() {
+    static async loadProducts() {
         try {
-            const data = await fs.readFile(ProductManager.#path, 'utf-8');
+            const data = await fs.promises.readFile(ProductManager.#path, 'utf-8');
             ProductManager.#products = JSON.parse(data) || [];
             console.log("se cargaron los productos desde el json un total "+ ProductManager.#products.length+" productos.")
         } catch (err) {
@@ -37,7 +30,7 @@ export class ProductManager{
             ProductManager.#products = [];
         }
     }
-
+    
     //Nos devuelve el valor que hay que asignar al ultimo id
     static UltimoId (){
         if(this.#products.length == 0){
@@ -49,26 +42,27 @@ export class ProductManager{
         let ultimoId = this.#products[ultimoElemento].id
         return ultimoId +1
     }
-
+    
     //Carga un producto al arreglo de productos
     static async addProduct(product){
-
+        
         //Verifico que tenga un titulo el producto
-        if(product.tittle == ""){
+        if( product.tittle == ""){
             return console.log("El producto debe tener titulo")
         }
-
+        
         //Verifico si existe
-        let existe = ProductManager.#products.find(p=> p.tittle === product.tittle)
+        let existe = ProductManager.#products.find(p=> p.code === product.code)
         if(existe){
-            return console.log(`El producto ${product.tittle} ya existe`)
+            console.log(`El producto con el codigo ${product.code} ya existe`)
+            return false
         }
-
+        
         //Se asigna el ultimo id y se carga en el arreglo
         let ultimo= ProductManager.UltimoId()
         let newProduct={id:ultimo,...product}        
         ProductManager.#products.push(newProduct)
-
+        
         //Se carga en el archivo .json
         try {
             await fs.promises.writeFile(ProductManager.#path, JSON.stringify(ProductManager.#products, null, 5), 'utf-8');
@@ -76,29 +70,53 @@ export class ProductManager{
         } catch (err) {
             console.error("Hubo un error al guardar el archivo:", err);
         }
-        
+        return true
     }
-    
-    //Trae todo los productos cargados
-    // static getProducts(){
-    //     ProductManager.#products.forEach(producto => {
-    //         console.log(producto)
-    //     });
-    // }
+
+    static async getProductById(id){
+        let indice= this.#products.findIndex(p=> p.id === id)
+        if(indice == -1){
+            return indice
+        }
+        return this.#products[indice]
+    }
 
     //Busca un Producto por id
-    static async getProductById(id){
-        let encontro= false
-        let products= await fs.promises.readFile(this.#path,{encoding:"utf-8"})
+    // static async getProductById(id){
+    //     let encontro= false
+    //     let products= await fs.promises.readFile(this.#path,{encoding:"utf-8"})
 
-        products.forEach(producto=>{
-            if(producto.id == id){
-                encontro=true
-                return console.log(`se encontro el producto es :"${producto.tittle}"`)
-            }
-        })
-        if (!encontro){
-            return console.error("Not found")
+    //     products.forEach(producto=>{
+    //         if(producto.id == id){
+    //             encontro=true
+    //             return console.log(`se encontro el producto es :"${producto.tittle}"`)
+    //         }
+    //     })
+    //     if (!encontro){
+    //         return console.error("Not found")
+    //     }
+    // }
+
+    static async modifyProduct(id,product={}){
+        //Verifico si existe
+        let existe = ProductManager.#products.find(p=> p.id === id)
+        if(!existe){
+            console.log(`El producto ${product.tittle} no existe`)
+            return false 
+        }
+        //Busco el indice
+        let indice=ProductManager.#products.findIndex( p => p.id === id)
+        //Creo el nuevo producto
+        let newProduct= {...ProductManager.#products[indice],...product}
+        //Reemplazo valores en el indice del arreglo por el nuevo producto
+        this.#products[indice]= {...this.#products[indice],...newProduct}
+        try{
+            //Escribo en el archivo json el nuevo producto
+            await fs.promises.writeFile(this.#path, JSON.stringify(ProductManager.#products, null, 5), 'utf-8')
+            return true
+        }catch(err){
+            //Devuelvo un error en caso de haberlo al escribir con fs
+            return console.log('error el escribir en el archvio, error :'+err)
         }
     }
 
@@ -110,7 +128,7 @@ export class ProductManager{
 
             //Se actualiza el archivo products.json
             try {
-                await fs.writeFile(ProductManager.#path, JSON.stringify(ProductManager.#products, null, 5), 'utf-8');
+                await fs.promises.writeFile(ProductManager.#path, JSON.stringify(ProductManager.#products, null, 5), 'utf-8');
                 console.log("Producto agregado y guardado en el archivo");
             } catch (err) {
                 console.error("Hubo un error al guardar el archivo:", err);
@@ -121,3 +139,19 @@ export class ProductManager{
     }
 
 }
+
+
+ // static async setProducts(product={}){
+    //     //validation
+    //     console.log(product)
+    //     let products = await fs.promises.readFile(this.#path, {encoding:'utf-8'})
+        
+    //     return products
+    // }
+    
+    //Trae todo los productos cargados
+    // static getProducts(){
+    //     ProductManager.#products.forEach(producto => {
+    //         console.log(producto)
+    //     });
+    // }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,26 +3,59 @@ import { ProductManager } from "../dao/productManager.js";
 
 export const router = Router()
 
+ProductManager.loadProducts();
+
 router.get("/",async(req,res)=>{
     let products= await ProductManager.getProducts()
-    res.status(200).send(console.log(products))
+    console.log(products)
+    res.status(200).send(products)
 })
 router.get("/:id", async(req,res)=>{
     let {id}= req.params
-    let product =  await ProductManager.getProductById(id)
-    res.status(200).send(product)
+    let product =  await ProductManager.getProductById(Number(id))
+    if(product == -1){
+        res.status(400).send('No existe un producto con id '+id)
+    }else{
+        res.status(200).send(product)
+    }
 })
 
 router.post("/", async (req,res)=>{
-    let {tittle, description, price, thumbnail, code, stock}=req.body
-    let product= {tittle:tittle,description:description,price:price,thumbnail:thumbnail,code:code,stock:stock}
+    let {tittle, description, code, price, stock, category,thumbnail}=req.body
+    let product= {tittle:tittle,description:description,code:code, price:price, status:true, stock:stock, category:category, thumbnail:thumbnail}
     try{
-        let products= await ProductManager.addProduct(product)
-        res.status(200).send('Se cargo el producto: '+ JSON.stringify(product) + 'en Products : ')
+        let result= await ProductManager.addProduct(product)
+        if(!result){
+            res.status(400).send('Ya existe un producto con el code '+code+' cargado')
+        }else{
+            res.status(200).send('Se cargo el producto: '+ JSON.stringify(product))
+        }
     }
     catch(err){
-        res.status(401).send('error al cargar el producto: '+ JSON.stringify(product) + ' error : '+err)
+        res.status(400).send('error al cargar el producto: '+ JSON.stringify(product) + ' error : '+err)
     }
     
+})
+
+router.put("/:id", async (req,res)=>{
+    let {id}= req.params
+    let {tittle, description, code, price, stock, category,thumbnail}=req.body
+    if(tittle==''){
+        res.status(400).send('Debe ingresar un titulo')
+    }else{
+        let product= {tittle:tittle,description:description,code:code, price:price, status:true, stock:stock, category:category, thumbnail:thumbnail}
+        try{
+            let result= await ProductManager.modifyProduct(Number(id),product)
+            if (!result){
+                res.status(400).send('No existe el producto con id :'+id)
+            }else{
+                res.status(200).send('Se modifico el producto: '+ JSON.stringify(product))
+            }
+        }
+        catch(err){
+            res.status(401).send('error al cargar el producto: '+ JSON.stringify(product) + ' error : '+err)
+        }
+        
+    }
 })
 
